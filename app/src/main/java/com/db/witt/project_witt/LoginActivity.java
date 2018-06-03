626a6776404ae3606e62102ba801b663be47cfd4
이동영 <ehrrnno17@naver.com>
2018-06-03 23:11:05 +0900

    회원 가입, 로그인, 비밀번호 암호화 완료

--- app/src/main/java/com/db/witt/project_witt/LoginActivity.java
+++ app/src/main/java/com/db/witt/project_witt/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.db.witt.project_witt;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,14 +10,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
-    EditText userID;
-    EditText userPassword;
-    TextView findButton;
-    Button loginBt;
-    Button registerButton;
-
+    private AlertDialog dialog;
     private long lastTimeBackPressed;
 
     @Override
@@ -24,21 +26,52 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        userID = (EditText)findViewById(R.id.idText);
-        userPassword = (EditText)findViewById(R.id.passwordText);
+        final EditText idText = (EditText)findViewById(R.id.idText);
+        final EditText passwordText = (EditText)findViewById(R.id.passwordText);
 
-        loginBt = (Button)findViewById(R.id.login); // 로그인 버튼
+        final Button loginBt = (Button)findViewById(R.id.login); // 로그인 버튼
         loginBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class); // Intent 전달을 이용한 액티비티 전환
-                LoginActivity.this.startActivity(loginIntent);
-                // TODO!: 로그인 인증 (프로그레스 바 추가)
-                // TODO!: 로그인 인증 완료 시 메인 액티비티로 이동, 실패 시 에러 메시지 출력
+                String userEmail = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("로그인에 성공했습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class); // Intent 전달을 이용한 액티비티 전환
+                                LoginActivity.this.startActivity(loginIntent);
+                                finish();
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                dialog = builder.setMessage("계정을 다시 확인해주세요.")
+                                        .setNegativeButton("다시 시도", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userEmail, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
         });
 
-        registerButton = (Button)findViewById(R.id.Sign_up); // 회원가입 버튼
+        final Button registerButton = (Button)findViewById(R.id.Sign_up); // 회원가입 버튼
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,7 +80,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        findButton = (TextView)findViewById(R.id.findpasswordButton); // 패스워드 찾기 버튼
+        final TextView findButton = (TextView)findViewById(R.id.findpasswordButton); // 패스워드 찾기 버튼
         findButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,6 +88,15 @@ public class LoginActivity extends AppCompatActivity {
                 LoginActivity.this.startActivity(findIntent);
             }
         });
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(dialog != null){
+            dialog.dismiss();
+            dialog = null;
+        }
     }
 
     @Override
--- app/src/main/java/com/db/witt/project_witt/RegisterActivity.java
+++ app/src/main/java/com/db/witt/project_witt/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.db.witt.project_witt;
 
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,64 +14,168 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
-    ArrayAdapter adapter;
-    Spinner spinner;
-    Button registerButton;
-    EditText idText;
-    EditText textPassword;
-    EditText nicknameText;
-    RadioGroup genderGroup;
-    RadioButton selectedGender;
+import org.json.JSONObject;
 
-    private String Uid, userID, userPassword, userNickname, userGender, userAge, userType;
+public class RegisterActivity extends AppCompatActivity {
+
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+
+    private String userEmail, userPassword, userGender, userNickname, userAge;
+
+    private AlertDialog dialog;
+    private boolean validate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        idText = (EditText)findViewById(R.id.idText);
-        textPassword = (EditText)findViewById(R.id.passwordText);
-        nicknameText = (EditText)findViewById(R.id.nicknameText);
-
-        genderGroup = (RadioGroup)findViewById(R.id.genderGroup);
-
-        registerButton = (Button)findViewById(R.id.submitButton);
-        registerButton.setOnClickListener(this);
-
         spinner = (Spinner)findViewById(R.id.AgeSpinner);
         adapter = ArrayAdapter.createFromResource(this, R.array.Age, android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-    }
 
-    private void CreateUser(){
-        // TODO!: 유저 객체 생성 및 DB 등록
-    }
+        final EditText idText = (EditText) findViewById(R.id.idText);
+        final EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        final EditText nicknameText = (EditText) findViewById(R.id.nicknameText);
 
-    private void registerUser(){
-        userID = idText.getText().toString().trim();
-        userPassword = textPassword.getText().toString().trim();
-        userNickname = nicknameText.getText().toString().trim();
+        RadioGroup genderGroup = (RadioGroup) findViewById(R.id.genderGroup);
+        int genderGroupID = genderGroup.getCheckedRadioButtonId();
+        userGender = ((RadioButton) findViewById(genderGroupID)).getText().toString();
 
-        if (TextUtils.isEmpty(userID)){
-            Toast.makeText(this, "ID를 입력해주세요.", Toast.LENGTH_SHORT).show();
-        }
+        genderGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton genderButton = (RadioButton) findViewById(i);
+                userGender = genderButton.getText().toString();
+            }
+        });
 
-        else if (TextUtils.isEmpty(userPassword)){
-            Toast.makeText(this, "Password를 입력해주세요.", Toast.LENGTH_SHORT).show();
-        }
+        final Button validateButton = (Button) findViewById(R.id.validateButton);
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = idText.getText().toString();
+                if(validate){
+                    return;
+                }
+                if(userEmail.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("아이디는 빈 칸일 수 없습니다.")
+                            .setPositiveButton("확인", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
 
-        else if (TextUtils.isEmpty(userNickname)){
-            Toast.makeText(this, "닉네임을 입력해주세요.", Toast.LENGTH_SHORT).show();
-        }
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("사용할 수 있는 아이디 입니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                idText.setEnabled(false);
+                                validate = true;
+                                idText.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                                validateButton.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("사용할 수 없는 아이디 입니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ValidateRequest validateRequest = new ValidateRequest(userEmail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(validateRequest);
+            }
+        });
 
-        // TODO!: 회원 가입 인증 구현 및 에러 메세지 출력
+        Button registerButton = (Button) findViewById(R.id.submitButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = idText.getText().toString();
+                String userPassword = passwordText.getText().toString();
+                String userNickname = nicknameText.getText().toString();
+                String userAge = spinner.getSelectedItem().toString();
+
+                if(!validate){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("먼저 중복체크를 해주세요.")
+                            .setPositiveButton("확인", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+
+                if (userEmail.equals("") || userPassword.equals("") || userNickname.equals("") || userAge.equals("") || userGender.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                    dialog = builder.setMessage("빈칸 없이 입력해주세요.")
+                            .setPositiveButton("확인", null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success){
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("회원 등록에 성공했습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                finish();
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                dialog = builder.setMessage("회원 등록에 실패했습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                RegisterRequest registerRequest = new RegisterRequest(userEmail, userPassword, userGender, userNickname, userAge, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
     }
 
     @Override
-    public void onClick(View view) {
-        registerUser(); // 회원 가입
+    protected void onStop(){
+        super.onStop();
+        if(dialog != null){
+            dialog.dismiss();
+            dialog = null;
+        }
     }
 }
