be520536135066eba222b08deeac97c8b04e646f
이동영 <ehrrnno17@naver.com>
2018-06-06 03:41:51 +0900

    좋아요, 내 리뷰, 좋아요 기능 추가

--- app/src/main/java/com/db/witt/project_witt/LikeActivity.java
+++ app/src/main/java/com/db/witt/project_witt/LikeActivity.java
@@ -1,8 +1,11 @@
 package com.db.witt.project_witt;
 
 import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -16,8 +19,12 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LikeActivity extends AppCompatActivity {
+
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,7 @@ public class LikeActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute(){
             try {
-                target = "http://ec2-13-209-75-74.ap-northeast-2.compute.amazonaws.com/UserLikeList.php?userEmail=" + URLEncoder.encode(MainActivity.userEmail,"UTF-8");
+                target = "http://ec2-13-209-75-74.ap-northeast-2.compute.amazonaws.com/UserReviewList.php?userEmail=" + URLEncoder.encode(MainActivity.userEmail,"UTF-8");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -71,14 +78,57 @@ public class LikeActivity extends AppCompatActivity {
         @Override
         public void onPostExecute(String result){
             try{
+
+                RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(layoutManager);
+
+                ArrayList<HashMap<String,String>> review_list = new ArrayList<HashMap<String, String>>();
+
                 JSONObject jsonObject = new JSONObject(result);
-                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                JSONArray jsonArray = jsonObject.getJSONArray("result");
 
                 int count = 0;
                 while(count<jsonArray.length()){
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    String user_email = object.getString("review_userEmail");
+                    String toilet_name = object.getString("toilet_name");
+                    String write_date = object.getString("write_date");
+                    String good_review = object.getString("good_review");
+                    String bad_review = object.getString("bad_review");
+                    String rating = object.getString("review_rating");
+                    String review_id = object.getString("review_id");
+
+                    HashMap<String, String> reviews = new HashMap<String, String>();
+
+                    reviews.put("user_email", user_email);
+                    reviews.put("toilet_name", toilet_name);
+                    reviews.put("write_date", write_date);
+                    reviews.put("good_content", good_review);
+                    reviews.put("bad_content", bad_review);
+                    reviews.put("rating", rating);
+                    reviews.put("current_userEmail","bb");
+                    reviews.put("toilet_id","aa");
+                    reviews.put("review_id",review_id);
+
+                    review_list.add(reviews);
 
                     count++;
                 }
+
+                if (review_list.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LikeActivity.this);
+                    dialog = builder.setMessage("작성한 리뷰가 없습니다.\n리뷰를 작성해주세요.")
+                            .setPositiveButton("확인", null)
+                            .create();
+                    dialog.show();
+                }
+
+                Review_Detail_Adapter review_detail_adapter = new Review_Detail_Adapter(LikeActivity.this,review_list);
+                recyclerView.setAdapter(review_detail_adapter);
+                review_detail_adapter.notifyDataSetChanged();
+
             }catch (Exception e){
                 e.printStackTrace();
                 Log.i("Target:",target);
