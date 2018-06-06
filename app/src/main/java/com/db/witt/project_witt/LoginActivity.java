c5cbda8d072942c202e5d6d69f6c01f71821d2db
JYEJI <jeongyeji0604@gmail.com>
2018-06-06 11:53:29 +0900

    로그인 유지

--- app/src/main/java/com/db/witt/project_witt/LoginActivity.java
+++ app/src/main/java/com/db/witt/project_witt/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.db.witt.project_witt;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,15 +22,37 @@ public class LoginActivity extends AppCompatActivity {
     private AlertDialog dialog;
     private long lastTimeBackPressed;
 
+    private String id;
+    private String pwd;
+
+    private boolean saveLoginData;
+    private SharedPreferences appData;
+
+    EditText idText;
+    EditText passwordText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText idText = (EditText)findViewById(R.id.idText);
-        final EditText passwordText = (EditText)findViewById(R.id.passwordText);
-
+        idText = (EditText)findViewById(R.id.idText);
+        passwordText = (EditText)findViewById(R.id.passwordText);
         final Button loginBt = (Button)findViewById(R.id.login); // 로그인 버튼
+
+        appData = getSharedPreferences("appData", MODE_PRIVATE);
+        load();
+        // 이전에 로그인 정보를 저장시킨 기록이 있다면
+        if (saveLoginData) {
+            idText.setText(id);
+            passwordText.setText(pwd);
+            Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class); // Intent 전달을 이용한 액티비티 전환
+            loginIntent.putExtra("userEmail", id);
+            loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            LoginActivity.this.startActivity(loginIntent);
+            finish();
+        }
+
         loginBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,6 +66,7 @@ public class LoginActivity extends AppCompatActivity {
                             JSONObject jsonObject = new JSONObject(response);
                             boolean success = jsonObject.getBoolean("success");
                             if(success){
+                                save();
                                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
                                 dialog = builder.setMessage("로그인에 성공했습니다.")
                                         .setPositiveButton("확인", null)
@@ -109,5 +133,29 @@ public class LoginActivity extends AppCompatActivity {
         }
         Toast.makeText(this,"뒤로가기 버튼을 한번 더 눌러 종료합니다.",Toast.LENGTH_SHORT).show();
         lastTimeBackPressed = System.currentTimeMillis(); // 로그인 화면에서 뒤로가기 키를 두번 누르면 앱 종료;
+    }
+
+    // 설정값을 저장하는 함수
+    private void save() {
+        // SharedPreferences 객체만으론 저장 불가능 Editor 사용
+        SharedPreferences.Editor editor = appData.edit();
+
+        // 에디터객체.put타입( 저장시킬 이름, 저장시킬 값 )
+        // 저장시킬 이름이 이미 존재하면 덮어씌움
+        editor.putBoolean("SAVE_LOGIN_DATA",true);
+        editor.putString("ID", idText.getText().toString().trim());
+        editor.putString("PWD", passwordText.getText().toString().trim());
+
+        // apply, commit 을 안하면 변경된 내용이 저장되지 않음
+        editor.apply();
+    }
+
+    // 설정값을 불러오는 함수
+    private void load() {
+        // SharedPreferences 객체.get타입( 저장된 이름, 기본값 )
+        // 저장된 이름이 존재하지 않을 시 기본값
+        saveLoginData = appData.getBoolean("SAVE_LOGIN_DATA", false);
+        id = appData.getString("ID", "");
+        pwd = appData.getString("PWD", "");
     }
 }
--- app/src/main/java/com/db/witt/project_witt/SettingActivity.java
+++ app/src/main/java/com/db/witt/project_witt/SettingActivity.java
@@ -1,12 +1,20 @@
 package com.db.witt.project_witt;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -25,7 +33,14 @@ public class SettingActivity extends AppCompatActivity {
                         .setCancelable(false) .setPositiveButton("확인", new DialogInterface.OnClickListener() {
                             @Override public void onClick(DialogInterface dialog, int which)
                             {
+                                SharedPreferences appData = getSharedPreferences("appData", Activity.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = appData.edit();
+                                //editor.clear()는 appData에 들어있는 모든 정보를 기기에서 지웁니다.
+                                editor.clear();
+                                editor.commit();
                                 Intent logoutIntent = new Intent(SettingActivity.this, LoginActivity.class); // Intent 전달을 이용한 액티비티 전환
+                                logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                logoutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                 SettingActivity.this.startActivity(logoutIntent);
                                 finish();
                             }
@@ -51,6 +66,38 @@ public class SettingActivity extends AppCompatActivity {
                     @Override public void onClick(DialogInterface dialog, int which)
                     {
                         // 회원 탈퇴 Request
+                        Response.Listener<String> responseListener = new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try{
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    boolean success = jsonObject.getBoolean("success");
+                                    if(success){
+                                        SharedPreferences appData = getSharedPreferences("appData", Activity.MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = appData.edit();
+                                        //editor.clear()는 appData에 들어있는 모든 정보를 기기에서 지웁니다.
+                                        editor.clear();
+                                        editor.commit();
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(SettingActivity.this);
+                                        AlertDialog dialog = builder.setMessage("탈퇴 되었습니다.")
+                                                .setPositiveButton("확인", null)
+                                                .create();
+                                        dialog.show();
+                                        Intent leaveIntent = new Intent(SettingActivity.this, SplashActivity.class); // Intent 전달을 이용한 액티비티 전환
+                                        leaveIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        leaveIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        SettingActivity.this.startActivity(leaveIntent);
+                                        finish();
+                                    }
+                                }
+                                catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        };
+                        DeleteUserRequest loginRequest = new DeleteUserRequest(MainActivity.userEmail, responseListener);
+                        RequestQueue queue = Volley.newRequestQueue(SettingActivity.this);
+                        queue.add(loginRequest);
                     }
                 })
                         .setNegativeButton("취소", new DialogInterface.OnClickListener() {
